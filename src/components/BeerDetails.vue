0fb5c296898940bcb85d4b1e4079db9e74b101f0
dafyddllyr <dafyddllyr@gmail.com>
2019-06-22 13:57:55 +0100

    Beer coordinates reaching beer details

--- src/components/BeerDetails.vue
+++ src/components/BeerDetails.vue
@@ -1,12 +1,23 @@
 <template lang="html">
   <div id="beer-details">
     <p>Beer Details</p>
+    <p> {{ beer.fields.name }}</p>
+    <p> {{ beer.geometry.coordinates }}</p>
   </div>
 </template>
 
 <script>
+import { eventBus } from '../main.js'
 export default {
-  name: 'beer-details'
+  name: 'beer-details',
+  data() {
+    return {
+      beer: ''
+    }
+  },
+  mounted() {
+    eventBus.$on('selected-beer', beer => this.beer = beer)
+  }
 }
 </script>
 
--- src/components/BeerList.vue
+++ src/components/BeerList.vue
@@ -2,14 +2,27 @@
   <div id="beer-list">
     <h3>BeerList</h3>
     <ul>
-      <li v-for="(beer, index) in filteredBeers" :key="index">{{ beer.fields.name }}</li>
-    </ul>
-  </div>
+      <li
+      v-for="(beer, index) in filteredBeers"
+      :key="index"
+      v-on:click="handleClick(beer)">
+      {{ beer.fields.name }}
+    </li>
+  </ul>
+</div>
 </template>
 
 <script>
+import { eventBus } from '../main.js'
+
 export default {
   name: 'beer-list',
+  methods: {
+    handleClick: function(beer) {
+      console.log(beer);
+      eventBus.$emit('selected-beer', beer)
+    }
+  },
   props: ['filteredBeers']
 }
 </script>
