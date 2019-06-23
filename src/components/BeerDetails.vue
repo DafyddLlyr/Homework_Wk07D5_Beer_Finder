4ac526022ba182e84c453cc45d5a31b178cb3358
dafyddllyr <dafyddllyr@gmail.com>
2019-06-23 10:08:08 +0100

    Basic implementation of vue2-leaflet completed

--- src/components/BeerDetails.vue
+++ src/components/BeerDetails.vue
@@ -1,32 +1,61 @@
 <template lang="html">
-  <div id="beer-details" v-if="beer">
-    <p>Beer Details</p>
-    <p> {{ beer.fields.name }}</p>
-    <p> {{ beer.geometry.coordinates }}</p>
+  <div id="beer-details">
+    <h3 class="beer-header"> {{ beer.fields.name }}</h3>
+    <table>
+      <tr>
+        <th>Style: </th>
+        <td>{{ beer.fields.style_name }}</td>
+      </tr>
+      <tr>
+        <th>Category: </th>
+        <td>{{ beer.fields.cat_name }}</td>
+      </tr>
+      <tr>
+        <th>ABV: </th>
+        <td>{{ beer.fields.abv.toFixed(2) }}%</td>
+      </tr>
+      <tr>
+        <th>Brewery: </th>
+        <td>{{ beer.fields.name_breweries.trim() }}</td>
+      </tr>
+      <tr>
+        <th>Location: </th>
+        <td>
+          <span v-if="beer.fields.state != undefined">{{ beer.fields.state }},  </span>{{ beer.fields.country }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
 import { eventBus } from '../main.js'
+
 export default {
   name: 'beer-details',
-  data() {
-    return {
-      beer: ''
-    }
-  },
-  mounted() {
-    eventBus.$on('selected-beer', beer => this.beer = beer)
-  }
+  props: ['beer']
 }
 </script>
 
 <style lang="css" scoped>
 
-#beer-details {
-  background-color: white;
-  width: 90%;
-  margin-top: 2vw;
+.beer-details .leaflet-popup-content-wrapper {
+  width: auto;
 }
+
+th {
+  padding-right: 1vw;
+}
+
+table {
+  cursor: default;
+}
+
+.beer-header {
+  cursor: default;
+  text-align: center;
+  margin: 1vw;
+  font-size: 1.1rem;
+}
+
 
 </style>
--- src/components/BeerMap.vue
+++ src/components/BeerMap.vue
@@ -1,50 +1,57 @@
-<template lang="html">
-  <div id="beer-map">
-  </div>
+<template>
+  <l-map style="height: 100%; width: 100%" :zoom="zoom" :center="center">
+    <l-tile-layer :url="url"></l-tile-layer>
+    <l-layer-group
+      layerType="overlay"
+      name="BeerMarkers">
+      <l-marker
+           v-for="(beer, index) in filteredBeers"
+           :key="index"
+           :lat-lng="[beer.fields.coordinates[0], beer.fields.coordinates[1]]"
+      >
+        <l-popup><beer-details :beer="beer"/></l-popup>
+      </l-marker>
+  </l-layer-group>
+  </l-map>
 </template>
 
 <script>
 import { keys } from '../../private/apikeys.js'
+import BeerDetails from './BeerDetails.vue'
+
 
 export default {
   name: 'beer-map',
   props: ['filteredBeers'],
-  data() {
+  data () {
     return {
       beerMap: null,
-      markers: null
+      markers: null,
+      url: `https://api.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=${keys['leaflet']}`,
+      zoom: 5,
+      center: [47.413220, -1.319482],
+      markerLatLng: [47.313220, -1.319482]
     }
   },
+  computed: {
+    bounds: function() {
+
+    }
+  },
+  components: {
+    'beer-details': BeerDetails
+  }
+}
+</script>
+
+<!-- <template lang="html">
+
+
   watch: {
     filteredBeers: function() {
       if (this.markers !== null) { this.markers.clearLayers() }
       let markerArray = this.filteredBeers.map(beer => {
-        let beerDetails =
-        `<div class='beer-details'>
-        <h3 class="beer-header">${beer.fields.name}</h3>
-        <table>
-          <tr>
-            <th>Style: </th>
-            <td>${beer.fields.style_name}</td>
-          </tr>
-          <tr>
-            <th>Category: </th>
-            <td>${beer.fields.cat_name}</td>
-          </tr>
-          <tr>
-            <th>ABV: </th>
-            <td>${beer.fields.abv.toFixed(2)}%</td>
-          </tr>
-          <tr>
-            <th>Brewery: </th>
-            <td>${beer.fields.name_breweries.trim()}</td>
-          </tr>
-          <tr>
-            <th>Location: </th>
-            <td>${beer.fields.state}, ${beer.fields.country}</td>
-          </tr>
-        </table>
-        `
+
         return L.marker(beer.fields.coordinates)
           .bindPopup(beerDetails).openPopup();
       })
@@ -52,43 +59,9 @@ export default {
       this.beerMap.addLayer(this.markers);
       this.beerMap.flyToBounds(this.markers.getBounds());
     }
-  },
-  mounted() {
-    this.beerMap = L.map('beer-map').setView([55.8634087,-4.2567716], 4);
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 18,
-      id: 'mapbox.streets',
-      accessToken: keys['leaflet']
-    }).addTo(this.beerMap);
   }
-}
-</script>
 
 <style lang="css">
 
-#beer-map, .container {
-  width: 100%;
-  height: 100%;
-}
 
-.beer-details .leaflet-popup-content-wrapper {
-  width: auto;
-}
-
-th {
-  padding-right: 1vw;
-}
-
-table {
-  cursor: default;
-}
-
-.beer-header {
-  cursor: default;
-  text-align: center;
-  margin: 1vw;
-  font-size: 1.1rem;
-}
-
-</style>
+</style> -->
